dbd8295ec1121b68d6c69afb21422bbe6a1d3ac5
SamSamhuns <samhunsadamant@gmail.com>
2019-07-22 00:25:55 +0400

    Add full req and completed scrap script

--- scrap_midi/scrap_freemidi_org.py
+++ scrap_midi/scrap_freemidi_org.py
@@ -1,9 +1,16 @@
 import os
 import sys
 import time
+import traceback
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.options import Options
+
+from selenium.webdriver.support import ui
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
 
 if len(sys.argv) != 3:
     print("Usage ./scrap_freemidi_org.py <genre> <Headless State 1/0>")
@@ -54,10 +61,12 @@ def find_artists(genre):
     with open('./logs/error_log', 'a') as elog:
         try:
             artist_link_count = len(mainContentList)
-            for list_index in range(artist_link_count):
+            for list_index in range(150, artist_link_count):
                 # div containing artists
                 mainContent = driver.find_element_by_xpath(
                     "//div[@id='mainContent']")
+                artist_name = mainContent.find_elements_by_xpath(
+                    ".//div/a[2]")[list_index].text
                 # goto artist page
                 mainContent.find_elements_by_xpath(
                     ".//div/a[2]")[list_index].click()
@@ -69,26 +78,82 @@ def find_artists(genre):
                     ".//div[1]/div[@class='artist-container']/div[1]/div/div[@class='artist-song-cell']/span/a")
 
                 song_link_count = len(songContentList)
-                for song_list_index in range(song_link_count):
+                current_song_page = 2  # Due to the indexing of the elements, it starts with 2
+                song_list_index = 0
+                while song_list_index < song_link_count:
+                    # for song_list_index in range(initial_Start, song_link_count):
                     # div containing artist songs
                     mainContentSong = driver.find_element_by_xpath(
                         "//div[@id='mainContent']")
+                    time.sleep(0.7)  # To make sure page has loaded
                     # goto song download page
-                    song_download_link = mainContentSong.find_elements_by_xpath(".//div[1]/div[@class='artist-container']/div[1]/div/div[@class='artist-song-cell']")[song_list_index]
-                    song_download_link = song_download_link.find_element_by_xpath(".//span/a")
+                    try:
+                        song_download_link = mainContentSong.find_elements_by_xpath(
+                            ".//div[1]/div[@class='artist-container']/div[1]/div/div[@class='artist-song-cell']")[song_list_index]
+                    except:
+                        print(f"List index {song_list_index} out of range")
+                        song_list_index += 1
+                        continue
+                    song_download_link = song_download_link.find_element_by_xpath(
+                        ".//span/a")
+
+                    with open('logs/downloaded_songs.txt', 'a') as f:
+                        log = "artist = " + str(artist_name) + ", artist_list_index = " +\
+                            str(list_index) + ", song_list_index = " +\
+                            str(song_list_index) + ', ' + \
+                            song_download_link.text
+                        print(log, file=f)
+                        print(log)
 
                     # goto download page
                     song_download_link.click()
+                    time.sleep(0.4)
+                    try:
+                        midi_download_link_str = "//*[@id='downloadmidi']"
+                        midi_download_link = ui.WebDriverWait(driver, 10).until(
+                            EC.element_to_be_clickable((By.XPATH, midi_download_link_str)))
+                        midi_download_link.click()
+                    except:
+                        print("Download link absent")
+                        song_list_index += 1
+                        continue
+                    time.sleep(0.5)
+                    song_list_index += 1
 
-                    midi_download_link = driver.find_element_by_id('downloadmidi').click()
-                    time.sleep(2)
+                    driver.back()  # Go back to song list page for artist
 
-                    driver.back()
+                    # If we are at the last song, check if another song page exists
+                    if song_list_index == (song_link_count - 1):
+                        page_lists = driver.find_elements_by_xpath(
+                            "//div[@id='mainContent']/div[1]/div[2]/div[2]/nav/ul/li")
+                        page_count = len(page_lists)
+                        if page_count >= 4 and current_song_page < page_count:  # if page_count == 3 then only one page exists
+                            current_song_page += 1
+                            next_page_string = "//div[@id='mainContent']/div[1]/div[2]/div[2]/nav/ul/li[" \
+                                + str(current_song_page) + ']/a'
+                            next_page = ui.WebDriverWait(driver, 10).until(
+                                EC.element_to_be_clickable((By.XPATH, next_page_string)))
+
+                            # go to the next page
+                            next_page.click()
+                            # div containing artist songs
+                            mainContentSong = driver.find_element_by_xpath(
+                                "//div[@id='mainContent']")
+                            songContentList = mainContentSong.find_elements_by_xpath(
+                                ".//div[1]/div[@class='artist-container']/div[1]/div/div[@class='artist-song-cell']/span/a")
+
+                            song_link_count = len(songContentList)
+                            song_list_index = 0
+
                 driver.back()
         except Exception as ex:
-            template = time.time() + "An exception of type {0} occurred. Arguments:\n{1!r}"
+            template = "Unix Time: " + \
+                str(time.time()) + \
+                ": An exception of type {0} occurred. Arguments:\n{1!r}"
+            traceback.print_exc()
             message = template.format(type(ex).__name__, ex.args)
             elog.write((message + '\n'))
+
 
 def main():
     # Get the music genre
