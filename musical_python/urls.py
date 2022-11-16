99513f499c145dd1280d16768af9d678fa397b4a
SamSamhuns <samhunsadamant@gmail.com>
2022-11-16 22:15:43 +0400

    Fix for new Django ver

--- musical_python/urls.py
+++ musical_python/urls.py
@@ -1,11 +1,11 @@
-from django.conf.urls import url
+from django.urls import re_path
 from django.contrib import admin
 from musical_python import views
 
 urlpatterns = [
-  url(r'^submit', views.submit),
-  url(r'^admin/', admin.site.urls),
-  url(r'^$', views.home, name='home'),
-  url(r'^instruments', views.instruments, name='instruments'),
-  url(r'^references', views.references, name='references'),
+    re_path(r'^submit', views.submit),
+    re_path(r'^admin/', admin.site.urls),
+    re_path(r'^$', views.home, name='home'),
+    re_path(r'^instruments', views.instruments, name='instruments'),
+    re_path(r'^references', views.references, name='references'),
 ]
